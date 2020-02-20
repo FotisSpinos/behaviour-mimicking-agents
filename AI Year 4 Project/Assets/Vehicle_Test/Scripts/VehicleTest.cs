d96c23352aca923dade7495d76eba0f0416c29d6
FotisSpinos <FSPINO200@CALEDONIAN.AC.UK>
2020-02-20 13:19:21 +0000

    Vehicle implementation
    Introduced
    -Vehicle test scene
    -Vehicle Script
    -Vehicle prefub

---
+++ AI Year 4 Project/Assets/Vehicle_Test/Scripts/VehicleTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleTest : MonoBehaviour
+{
+    [SerializeField] private Vehicle vehicle;
+
+    private void Update()
+    {
+        vehicle.SetVehicleInput(Input.GetAxis("Horizontal"),
+            Input.GetAxis("Vertical"));
+    }
+}
