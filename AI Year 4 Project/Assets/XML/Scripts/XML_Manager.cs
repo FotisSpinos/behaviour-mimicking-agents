6e7d00cf3fb20cb4a1dcf99db9ef578ffdd7e7eb
WDonald <WDONAL200@CALEDONIAN.AC.UK>
2020-03-03 21:59:52 +0000

    test

---
+++ AI Year 4 Project/Assets/XML/Scripts/XML_Manager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using System;
+
+public class XML_Manager : MonoBehaviour
+{
+    public static XML_Manager manager;
+
+    public AgentStates agentStates;
+    public Agent_State agent;
+
+    public string fileName;
+
+    void Awake()
+    {
+        manager = this;
+    }
+
+    public void AddState(Agent_State state)
+    {
+        agentStates.states.Add(state);
+    }
+
+    public List<Agent_State> GetList_AgentStates()
+    {
+        return agentStates.states;
+    }
+
+    public void Save()
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(AgentStates));
+        FileStream stream = new FileStream(Application.dataPath + "/Data/Xml/" + fileName + ".xml", FileMode.Create); //Filemode Create ovewrites
+
+        serializer.Serialize(stream, agentStates);
+        stream.Close();
+
+        Debug.Log("Save");
+    }
+
+    public void Load()
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(AgentStates));
+        FileStream stream = new FileStream(Application.dataPath + "/Data/Xml/" + fileName + ".xml", FileMode.Open);
+
+        agentStates = serializer.Deserialize(stream) as AgentStates;
+        stream.Close();
+        
+        Debug.Log("Load");
+    }
+}
+
+[Serializable]
+public class Agent_State
+{
+    public Vector3 Position;
+    public Vector3 rotation;
+    public Vector3 velocity;
+}
+
+[Serializable]
+public class AgentStates
+{
+    public List<Agent_State> states = new List<Agent_State>();
+}
---
+++ AI Year 4 Project/Assets/XML/Scripts/XML_Test.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XML_Test : MonoBehaviour
+{
+    public XML_Manager manager_XML;
+
+    public Agent_State testState;
+
+    public List<Agent_State> loadedStats = new List<Agent_State>();
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.S))
+        {
+            Debug.Log("A");
+            manager_XML.AddState(testState);
+        }          
+        
+        if(Input.GetKeyUp(KeyCode.S))
+        {
+            Debug.Log("S");
+            manager_XML.Save();
+        }        
+        
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            Debug.Log("L");
+            manager_XML.Load();
+            loadedStats = manager_XML.GetList_AgentStates();
+        }
+    }
+}
